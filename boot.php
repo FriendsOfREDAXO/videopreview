9191914e50e0c7ef32103acb872747e75edb7b5a
Thomas Skerbis <thomas.skerbis@mac.com>
2024-12-05 17:50:59 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,4 +1,5 @@
 <?php 
 if (rex_addon::get('media_manager')->isAvailable()) {
     rex_media_manager::addEffect('rex_effect_video_to_webp');
+    rex_media_manager::addEffect('rex_effect_video_to_preview');
 }
